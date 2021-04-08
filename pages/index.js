362bfdf6c5825be64a98383a95312494cdcd14d1
asherjay <70672377+asherjay@users.noreply.github.com>
2021-04-09 01:19:12 +0200

    created pagination and search results page with more tailwind css

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,24 @@ import {MicrophoneIcon, ViewGridAddIcon, ViewGridIcon} from "@heroicons/react/so
 import Image from "next/image";
 import {SearchIcon} from "@heroicons/react/outline";
 import Footer from '../components/Footer';
+import { useRef } from 'react';
+import {useRouter} from 'next/router';
 
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef =useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if(!term) return;
+
+    router.push(`/search?term=${term}`)
+
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -46,13 +61,13 @@ export default function Home() {
     />
     <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg  max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xls"> 
       <SearchIcon className="h-5 mr-3 text-gray-500" />
-      <input type="text" className="flex-grow focus:outline-none" 
+      <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" 
      />
      <MicrophoneIcon className="h-5"/>
     </div>
     <div className="flex flex-col w1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-      <button className="btn">Google Search</button>
-    <button className="btn">I am feeling Lucky</button>
+    <button onClick={search} className="btn">Google Search</button>
+    <button onClick={search} className="btn">I am feeling Lucky</button>
     </div>
     </form>
 
