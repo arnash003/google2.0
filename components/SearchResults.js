d2c528b0c999aacab390e383a0109899b93c1009
asherjay <70672377+asherjay@users.noreply.github.com>
2021-04-20 11:12:01 +0200

    Finished NextJSgooglefrontendclone

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,10 +4,12 @@ function SearchResults({results}) {
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
             <p className="text-gray-600 text-md mb-5 mt-3">
-                About 
-            {results.searchInformation?.formattedTotalResults}
+              About 
+                  {results.searchInformation?.formattedTotalResults}
              results (
-                {results.searchInformation?.formattedSearchTime} seconds) 
+                {results.searchInformation?.formattedSearchTime}
+                
+             seconds ) 
             </p>
             {results.item?.map((result) => (
                 <div key={result.link} className="max-w-xl mb-8">
